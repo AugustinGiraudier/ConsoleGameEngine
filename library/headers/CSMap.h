cf2465be59699e63724c2cf985d575562d8b718b
augiraudie <augustin.giraudier@etu.uca.fr>
2021-04-26 20:27:46 +0200

    test lib

---
+++ library/headers/CSMap.h
@@ -0,0 +1,128 @@
+#ifndef CSMAP_H
+#define CSMAP_H
+
+#include <vector>
+#include <string>
+#include <sstream>
+#include <exception>
+
+
+/**
+ * \brief classe de gestion du buffer d'affichage.
+ */
+class CSMap {
+private:
+
+	/**
+	 * \brief Donnée brutes (tableau de char 2 dim).
+	 */
+	std::vector<std::vector<char>> _map;
+
+	/**
+	 * \brief Dimension x du buffer.
+	 */
+	unsigned int _Dimx;
+
+	/**
+	 * \brief Dimension y du buffer.
+	 */
+	unsigned int _Dimy;
+
+	CSMap(CSMap& other);
+
+public:
+
+	/*------------------------------------------------------------------------------*/
+	/*	CONSTRUCTEURS																*/
+	/*------------------------------------------------------------------------------*/
+
+	/**
+	 * \brief construit un buffer d'affichage paramétré.
+	 * 
+	 * \param DimX :		dimension du buffer en abscisses
+	 * \param DimY :		dimension du buffer en ordonnées
+	 * \param DefaultChar : caractère de remplissage
+	 */
+	CSMap(const unsigned int DimX, const unsigned int DimY, const char DefaultChar = ' ');
+
+	/**
+	 * \brief construit un buffer d'affichage vide.
+	 * 
+	 */
+	CSMap();
+
+	/*------------------------------------------------------------------------------*/
+	/*	OUTILS																		*/
+	/*------------------------------------------------------------------------------*/
+
+	/**
+	 * \brief Récupération de la dimension x du buffer.
+	 * 
+	 * \return la dimension x du buffer
+	 */
+	const unsigned int Get_Dim_X() const { return _Dimx; }
+
+	/**
+	 * \brief Récupération de la dimension y du buffer.
+	 * 
+	 * \return la dimension y du buffer.
+	 */
+	const unsigned int Get_Dim_Y() const { return _Dimy; }
+
+	/**
+	 * \brief Récupération du buffer sous forme de string.
+	 * 
+	 * \return le buffer d'affichage sous forme de string
+	 */
+	std::string Get_Str() const;
+	
+	/*------------------------------------------------------------------------------*/
+	/*	DESSIN																		*/
+	/*------------------------------------------------------------------------------*/
+
+	/**
+	 * \brief Permet de placer un charactère à des coordonnées dans le buffer d'affichage.
+	 *
+	 * \param c : caractère à insérer
+	 * \param x	: position x
+	 * \param y : position y
+	 */
+	void Set_Str_At(const std::string& str, unsigned int x, unsigned int y);
+
+	/**
+	 * \brief Permet de remplir le buffer d'affichage avec un caractère.
+	 *
+	 * \param c : caractère à insérer
+	 */
+	void Full_Fill(char c);
+
+	/**
+	 * \brief Permet de remplir une ligne du buffer d'affichage avec un caractère.
+	 *
+	 * \param c :		caractère à insérer
+	 * \param Row_Id :	index de la ligne à remplir
+	 */
+	void Set_Char_At_Row(char c, unsigned int Row_Id);
+
+	/**
+	 * \brief Permet de remplir une colonne du buffer d'affichage avec un caractère.
+	 *
+	 * \param c :		caractère à insérer
+	 * \param Col_Id :	index de la colonne à remplir
+	 */
+	void Set_Char_At_Col(char c, unsigned int Col_Id);
+
+	/*------------------------------------------------------------------------------*/
+	/*	OPERATEURS																	*/
+	/*------------------------------------------------------------------------------*/
+
+	/**
+	 * \brief permet d'accéder à une colonne du buffer.
+	 * 
+	 * \param a : index de la colonne dans le buffer
+	 * \return la colonne à l'index a
+	 */
+	std::vector<char>& operator[](unsigned int a);
+	void operator=(const CSMap& other);
+};
+#endif
---
+++ library/headers/Hint.h
@@ -0,0 +1,77 @@
+#ifndef HINT_H
+#define HINT_H
+
+/**
+ * \brief Enum des différentes indications pouvant êtres appliquées.
+ */
+enum class Hint_Types {
+	Window_Size,	// Nouvelle taille de fenetre & buffer d'affichage
+	Update_Period,	// Nouvelle durée entre mises à jour d'affichage
+	Default_Char	// Nouveau caractère par défaut (pour le buffer d'affichage)
+};
+
+/** \brief Classe virtuelle pure servant de base aux classes d'indications */
+class Hint{
+private:
+	Hint_Types Type;
+public:
+	/**
+	 * \brief Permet de récupérer le type d'indication.
+	 */
+	virtual Hint_Types GetHintType() const = 0;
+	Hint(Hint_Types type);
+};
+
+class Hint_Window_Size : public Hint {
+public :
+	unsigned int SizeX;
+	unsigned int SizeY;
+
+	/**
+	 * \brief création d'une indication de resize de la fenêtre et du buffer d'affichage.
+	 * 
+	 * \param Size_x : nouvelle taille en abscisses
+	 * \param Size_y : nouvelle taille en ordonnées
+	 */
+	Hint_Window_Size(unsigned int Size_x, unsigned int Size_y);
+
+	/**
+	* \brief Permet de récupérer le type d'indication.
+	*/
+	virtual Hint_Types GetHintType() const override;
+};
+
+class Hint_Update_Period : public Hint {
+public:
+	unsigned int Period;
+
+	/**
+	 * \brief création d'une indication de durée entre mises à jour d'affichage.
+	 * 
+	 * \param period : nouvelle durée en millisecondes
+	 */
+	Hint_Update_Period(unsigned int period);
+
+	/**
+	* \brief Permet de récupérer le type d'indication.
+	*/
+	virtual Hint_Types GetHintType() const override;
+};
+
+class Hint_Default_Char : public Hint {
+public:
+	char d_char;
+
+	/**
+	 * \brief création d'une indication de caractère par défaut.
+	 * 
+	 * \param dchar : caractère par défaut qui remplira le buffer d'affichage 
+	 */
+	Hint_Default_Char(char dChar);
+
+	/**
+	* \brief Permet de récupérer le type d'indication.
+	*/
+	virtual Hint_Types GetHintType() const override;
+};
+#endif // !HINT_H
